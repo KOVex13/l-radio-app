fdfe1bcfd7912d00dda0e8272a25baacb375b935
KOVex <kovex@portal2u.ru>
2023-03-22 21:59:10 +0500

    useKeepAwake

--- App.js
+++ App.js
@@ -4,11 +4,12 @@ import { StatusBar } from 'expo-status-bar';
 //import { StyleSheet, Text, View } from 'react-native';
 import { NativeBaseProvider, Text, Box } from "native-base";
 import { LinearGradient } from 'expo-linear-gradient';
+import { useKeepAwake } from 'expo-keep-awake';
 
 import LRadioApp from './src/l-radio-app.jsx';
 
 export default () => {
-
+	useKeepAwake();
 	const config = {
 		dependencies: {
 			'linear-gradient': LinearGradient,
